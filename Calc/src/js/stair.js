1e5423b28a21e1609386a201f050aeb372f86b73
ecSC2 <quantm00.chan@gmail.com>
2021-12-01 15:14:39 +0800

    added stair and step js

---
+++ Calc/src/js/stair.js
@@ -0,0 +1,19 @@
+<div class="subcontent-sub-info">
+  <p>
+    A twin-stile rung ladder comprises of two stiles and rungs that assists user ascending/descending between levels. It is to be inclined between 70° and 90°, and can be both portable or permanently
+    fixed to a structure. The Australia Standard AS1657 sets out requirements for the design, selection, construction and installation of ladder and is to be strictly followed for compliances. Please
+    contact certified supplier/installer or refer to Australian Standard AS1657 for more information.
+  </p>
+  <br />
+  <ul>
+    <li>
+      <strong>Vertical Height</strong> - Vertical measurement between levels (300mm - 6000mm)
+    </li>
+    <li>
+      <strong>Ladder Width</strong> - Desired width of the ladder measuring between stiles (375mm - 525mm)
+    </li>
+    <li>
+      <strong>Ladder Pitch</strong> - Desired slope of the ladder (70° - 90°)
+    </li>
+  </ul>
+</div>;
---
+++ Calc/src/js/stepLadder.js
@@ -0,0 +1,22 @@
+<div class="subcontent-sub-info">
+  <p>
+    A step ladder is similar to rung ladder where it comprises of two stiles and flat steps that assists user ascending/descending between levels, contra to round rungs. It is to be inclined between
+    60° and 70°, and can only be permanently fixed to a structure. The Australia Standard AS1657 sets out requirements for the design, selection, construction and installation of ladder and is to be
+    strictly followed for compliances. Please contact certified supplier/installer or refer to Australian Standard AS1657 for more information.
+  </p>
+  <br />
+  <ul>
+    <li>
+      <strong>Vertical Height</strong> - Vertical measurement between levels (300mm - 6000mm)
+    </li>
+    <li>
+      <strong>Ladder Width</strong> - Desired width of the ladder measuring between stiles (450mm - 750mm)
+    </li>
+    <li>
+      <strong>Ladder Pitch</strong> - Desired slope of the ladder (60° - 70°)
+    </li>
+    <li>
+      <strong>Handrail Clearance</strong> - Width measuring between handrails (550mm - 750mm)
+    </li>
+  </ul>
+</div>;
